500fd703923839922986d0578d322e36104b4625
MS Rahman <msrahman048gmail.com>
2024-04-29 22:20:39 +0600

    delete operation completed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-// const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -9,10 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ts8x6gb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-console.log(uri)
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,7 +23,59 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    await client.connect();
+
+    const touristsSpotCollect = client.db('touristsSpot').collection('touristsSpot');
+    const userCollect = client.db('touristsSpot').collection('user')
+
+
+
+
+    app.post('/TouristsSpot', async(req, res) =>{
+      const newTouristsSpot = req.body;
+      console.log('newTouristsSpot', newTouristsSpot);
+      const result = await touristsSpotCollect.insertOne(newTouristsSpot);
+      res.send(result)
+    })
+    app.get('/TouristsSpot', async(req, res) =>{
+      const cursor = touristsSpotCollect.find();
+
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+    app.get('/users', async(req, res) =>{
+      const cursor = userCollect.find();
+
+      const users = await cursor.toArray();
+      res.send(users)
+    })
+    app.get('/TouristsSpot/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+
+      const result = await touristsSpotCollect.findOne(query);
+      res.send(result)
+    })
+
+    app.delete('/TouristsSpot/:id', async(req, res) =>{
+      const id = req.params.id;
+      console.log('please delete from database', id);
+      const query = {_id: new ObjectId(id)}
+      const result = await touristsSpotCollect.deleteOne(query);
+      res.send(result)
+    })
+
+
+    // user related apis
+    app.post('/user', async(req, res) =>{
+      const user = req.body;
+      console.log(user);
+
+      const result = await userCollect.insertOne(user);
+      res.send(result)
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
